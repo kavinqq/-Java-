1aaa6db3e2493b5af92a862b36e86e20048eaf1f
KuanLin <36700601+kavinqq@users.noreply.github.com>
2022-01-14 02:00:29 +0800

    Update Chapter06-1.java

--- Chapter06-1.java
+++ Chapter06-1.java
@@ -1,1 +1,11 @@
-
+public class Main{
+  public static void main(String[] args) {
+    int sum = 0;
+    for(int i = 1; i <= 100; i ++){
+        if(i % 3 == 0){
+            sum += i;
+        }
+    }
+    System.out.println(sum);
+  }//end main()
+}//end class Main
